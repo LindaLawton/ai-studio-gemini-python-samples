27c280db8e971ef51777b729c7dd7e2de57c0be1
Linda Lawton <laurly71@gmail.com>
2024-12-29 15:12:10 +0100

    add more then one image example

--- Vision/genreate_text_from_multiple_images_and_text.py
+++ Vision/genreate_text_from_multiple_images_and_text.py
@@ -1,17 +1,17 @@
 from dotenv import load_dotenv
 import google.generativeai as genai
 import os
-import PIL.Image
+import base64
 load_dotenv()
 
 
-def get_response(text, image_path) -> str:
+def get_response(prompt: str, image_paths: [str]) -> str:
     """
     Generates a text response using the specified Google Generative AI model.
 
     Args:
-      text: The input text for the model.
-      image_path: The input image path for the model.
+      prompt: The input text for the model.
+      image_paths: The input image path for the model.
 
     Returns:
       The generated text response.
@@ -19,11 +19,28 @@ def get_response(text, image_path) -> str:
     genai.configure(api_key=os.getenv("API_KEY"))
     model = genai.GenerativeModel(os.getenv("TEXT_IMAGE_MODEL_NAME"))
 
-    image_data = PIL.Image.open(image_path)
-    response = model.generate_content([text, image_data])
-    return response.text
+    image_data = []
+    for image_path in image_paths:
+        try:
+            with open(image_path, "rb") as image_file:
+                image_data.append({
+                    'mime_type': 'image/jpeg',
+                    'data': base64.b64encode(image_file.read()).decode('utf-8')
+                })
+        except FileNotFoundError:
+            print(f"Error: Image file not found at {image_path}")
+
+    try:
+        response = model.generate_content(image_data + [prompt])
+        return response.text
+    except Exception as e:
+        print(f"Error generating response: {e}")
+        return None
+
+
 
 
 if __name__ == "__main__":
-    response_text = get_response("What do you see?", "../test_data/bird_image_snickers_2024.jpg")
+    response_text = get_response("What do you see?", ["../test_data/bird_image_snickers_2024.jpg",
+                                                      "../test_data/bread_image.jpg"])
     print(response_text)
